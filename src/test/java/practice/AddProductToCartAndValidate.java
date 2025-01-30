88f8968ebf5190a7ae32515b38c325e9e7f3adb6
Varshareshu <varshareshu1@gmail.com>
2025-01-30 23:33:19 +0530

    Initial

--- src/test/java/practice/AddProductToCartAndValidate.java
+++ src/test/java/practice/AddProductToCartAndValidate.java
@@ -16,7 +16,8 @@ public class AddProductToCartAndValidate {
 	public static void main(String[] args, Object productToBeAdded) {
 		
 		int x=0;
-		int vv=0;
+		int vv=0;   
+		int yy=0;
 		int y = 0;  
 		// step 1: Launch the browser
 		WebDriver driver = new EdgeDriver();
